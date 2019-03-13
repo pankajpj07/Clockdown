1bd1290c162741b4aed5abf69e581e7fc541c8e0
Pankaj Jain <47389183+pankajpj07@users.noreply.github.com>
2019-03-14 01:13:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-function submitForm()
-{
-  if (document.getElementById("name").value=="" || document.getElementById("email").value=="" || document.getElementById("message").value==""){
-    alert("All fields are required");
-  }
-  else
+var countDownDate = new Date("Mar 31, 2019 00:00:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var distance = countDownDate - now;
+if(distance<0)
     {
-  alert("Sent sucessfully");
+    
+    document.getElementById("days").textContent=0;
+    document.getElementById("hours").textContent=0;
+    document.getElementById("minutes").textContent=0;
+    document.getElementById("seconds").textContent=0;
+    clearInterval(x);
     }
-}
+    
+  // Time calculations for days, hours, minutes and seconds
+  	var s= Math.floor(distance/1000);
+  	var m=Math.floor(s/60);
+  	var h=Math.floor(m/60);
+  	var d=Math.floor(h/24);
+    h%=24;
+    m%=60;
+    s%=60;
+    h=(h<10)? "0" + h : h;
+    m=(m<10)? "0" + m : m;
+    s=(s<10)? "0" + s : s;
+    document.getElementById("days").textContent=d;
+    document.getElementById("hours").textContent=h;
+    document.getElementById("minutes").textContent=m;
+    document.getElementById("seconds").textContent=s;
+    
+
+},1000);
